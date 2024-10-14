3509e65ea3ef18916f38d4e0fcdc9dfafe1eee4f
Allan Calixto <allan.calixto10@hotmail.com>
2024-10-13 23:48:34 -0300

    add lixeiraController.js add caminhaoController.js

---
+++ AngularApp/caminhaoController.js
@@ -0,0 +1,11 @@
+﻿var app = angular.module('app', []);
+
+app.controller('caminhaoController', ['$scope', '$http', caminhaoController]);
+
+function caminhaoController($scope, $http) {
+    $http.get('https://localhost:7012/api/Caminhao/ListarCaminhoes').success(function (data) {
+        $scope.listaCaminhoes = data;
+    }).erro(function () {
+        $scope.erro = "Erro: Não foi possível carregar a listagem de Caminhoes.";
+    });
+}
